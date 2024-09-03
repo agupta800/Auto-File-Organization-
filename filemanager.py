54433c10735433245334fac19cf654bf4a01ae7f
Awadhesh Gupta <90196465+agupta800@users.noreply.github.com>
2024-09-03 14:31:24 +0545

    Update filemanager.py
    Auto File Organization helps to organize files quickly by moving all the files to its respective folders accordingly to their extension.

--- filemanager.py
+++ filemanager.py
@@ -2,6 +2,7 @@
 # # # # Name : Awadhesh Gupta
 # # # # Enrollment No. : 20SOECE11095
 # # # # Class : 3CEC
+# # # # Auto File Organization helps to organize files quickly by moving all the files to its respective folders accordingly to their extension.
 
 import os, shutil
 from os.path import join
